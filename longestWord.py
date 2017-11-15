5dc220696a338db232867bb415a79cfd67e825d7
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-11-15 13:59:10 -0500

    Updated from Brython Server: 11/15/2017 1:59:04 PM

--- longestWord.py
+++ longestWord.py
@@ -1,1 +1,13 @@
+#Morgan Baughman
+#11/15/17
+#longestWord.py - pirnt out the longest word
+
+word = input('Enter a list of words: ').split()
+
+w = 1
+word = ""
+length = len(w)
+for w in word:
+    if w in word <= 1:
+        print(length)
 
