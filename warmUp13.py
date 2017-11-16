a02607926a540d8119321256266d1cfae619cf8c
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-11-16 14:36:02 -0500

    Updated from Brython Server: 11/16/2017 2:36:01 PM

--- warmUp13.py
+++ warmUp13.py
@@ -1,1 +1,19 @@
+#Morgan Baughman
+#11/16/17
+#warmup13.py
 
+from random import randint
+
+numbers = []
+
+
+for i in range(1,20):
+    num = randint(1,1000)
+    numbers.append(num)
+    
+
+numbers.sort()
+
+print('The min is' , numbers[0])
+print('The max is' , numbers[-1])
+print('The sum is' , sum(numbers))
