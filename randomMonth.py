317dec74d3ddf52e1ad48394202fe99a80777d8d
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-11-16 14:54:52 -0500

    Updated from Brython Server: 11/16/2017 2:54:52 PM

--- randomMonth.py
+++ randomMonth.py
@@ -1,1 +1,13 @@
+#Morgan Baughman
+#11/16/17
+#randomMonth.py
 
+from random import randint
+
+months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'Sepetember', 'October', 'November', 'December]
+
+for i in range(1,12):
+    num = randint(1,12)
+    numbers(num)
+    
+print(numbers(num))
