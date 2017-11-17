f6b3787dbbf8fcfe2d1ea9896cad4eecf6fb7a15
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-11-17 14:32:30 -0500

    Updated from Brython Server: 11/17/2017 2:32:30 PM

--- matrixDemo.py
+++ matrixDemo.py
@@ -1,1 +1,18 @@
+#Morgan Baughman
+#11/17/17
+#matrixDemo.py
+
+board = [[‘a’,’b’,’c’],[‘d’,’e’,’f’],[‘g’,’h’,’i]]
+
+
+def printBoard()
+for row in range(0,3):
+    for col in range(0,3):
+        print(board[row][col],’ ‘,end= ‘’)
+    print()
+    
+printBoard()
+
+row =int(input(‘Enter a row number’))
+col = int(input(‘Enter a col number’))
 
