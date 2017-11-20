bda45f986a837ef22fd73b2b3006de6a7832bd6b
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-11-20 14:54:13 -0500

    Updated from Brython Server: 11/20/2017 2:54:13 PM

--- betterColorChangeWindow.py
+++ betterColorChangeWindow.py
@@ -2,34 +2,17 @@
 #11/17/17
 #betterColorChangeWindow.py
 
-
 from random import randint
 from ggame import *
 
-
-blue = Color(0x0000FF, 1)
-red = Color(0xFF0000, 1)
-green = Color(0x00FF00, 1)
-lightgreen = Color(0x33FF8D, 1)
-purple = Color(0xE033FF, 1)
-
-
 def mouseClick(event):
-    num = randint(1,4)
-    
-redRectangle = RectangleAsset(700, 700, LineStyle(1, red), red)
-lightgreenRectangle = RectangleAsset(700, 700, LineStyle(1, lightgreen), lightgreen)
-greenRectangle = RectangleAsset(700, 700, LineStyle(1, green), green)
-purpleRectangle = RectangleAsset(700, 700, LineStyle(1, purple), purple)
-blueRectangle = RectangleAsset(700, 700, LineStyle(1, blue), blue)
-
-Sprite(redRectangle)
-Sprite(blueRectangle)
-Sprite(lightgreenRectangle)
-Sprite(greenRectangle)
-Sprite(purpleRectangle)
+    colorList=['0xFF0000','0x0000FF','0x00FF00','0xE033FF','0x33FF8D']
+    color = Color(colorList[randint(1,5)-1], 1)
+    colorRectangle = RectangleAsset(1000, 1000, LineStyle(1, color), color)
 
 
+
+    Sprite(colorRectangle)
 App().listenMouseEvent("click", mouseClick)
 App().run()
 
