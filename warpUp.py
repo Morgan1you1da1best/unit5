80775cf4f51df21ba2909335498e4cead50f9fc8
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-11-30 14:28:41 -0500

    Updated from Brython Server: 11/30/2017 2:28:41 PM

--- warpUp.py
+++ warpUp.py
@@ -1,1 +1,14 @@
+#Morgan Baughman
+#11/30/17
+#warpUp15.py - write a function that takes a list of numbers as an arguement and returns another list where each number is doubled.
 
+
+def double(numbers):
+    numdouble = []
+    for num in numbers:
+        num = (num*2)
+        numdouble.append(num)
+    return numdouble
+
+print(double([2,4,6]))
+    
