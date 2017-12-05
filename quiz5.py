bba6393a143c60b3e2c26a8ce1f763b1ad3916ee
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-12-05 15:09:51 -0500

    Updated from Brython Server: 12/5/2017 3:09:50 PM

--- quiz5.py
+++ quiz5.py
@@ -1,1 +1,30 @@
+#Morgan Baughman
+#12/5/17
+#quiz.py
+
+from random import randint
+
+def wordLengths(word1,word2,word3,word4):
+    words = []
+    print(len(word1))
+    print(len(word2))
+    print(len(word3))
+    print(len(word4))
+
+def lastElement(thing1,thing2,thing3):
+    
+    return (thing3)
+    
+def rand5():
+    num = []
+    num.append(randint(1,100))
+    num.append(randint(1,100))
+    num.append(randint(1,100))
+    return num[:]
+
+
+print(rand5())
+print(lastElement('cat','dog','rat'))
+print(wordLengths('the','cat','is','hungry'))
+
 
